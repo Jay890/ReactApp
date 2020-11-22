14a68ff77cdab4302c71c4514ee1a8cb7a01b4c2
Jason Leong <42172421+Jay890@users.noreply.github.com>
2020-11-23 08:16:56 +1000

    Testing
    Testing components

--- react-starter/src/App.js
+++ react-starter/src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 // Import Component
-import TestComponent from './TestComponent'
+import TestComponent from "./TestComponent";
 
 function App() {
+  // simple JS code
+  const items = [1, 2, 3, 4, 5];
+
+  items.map((item) => {
+    console.log(item);
+  });
+
+  // JSX code
   return (
     <div className="box">
       <h1>Hello React</h1>
-      <TestComponent/>
+      <TestComponent />
+      <button>Submit</button>
     </div>
   );
 }
-
 
 export default App;
