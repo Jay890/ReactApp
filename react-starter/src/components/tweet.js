708ac5c21862ec369e0f6aa0622b2984e42f17de
Jason Leong <42172421+Jay890@users.noreply.github.com>
2020-11-24 07:37:12 +1000

    Testing
    Practice on using react

---
+++ react-starter/src/components/tweet.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Tweet = () => {
+  return (
+    <div>
+      <h2>Name</h2>
+      <p>This is the actual tweet</p>
+      <button>Edit</button>
+      <button>Like</button>
+    </div>
+  );
+};
+
+export default Tweet;
