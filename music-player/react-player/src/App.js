34d6caf30500bb958e04a298ba413884115e23f4
Jason Leong <42172421+Jay890@users.noreply.github.com>
2020-12-10 07:40:25 +1000

    Install uuid package and song information setup
    Array object that holds information about the songs, which is returned from function
    Setting state for the songs and passing down to Song component via props

--- music-player/react-player/src/App.js
+++ music-player/react-player/src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
 // Import Main Styles
 import "./styles/app.scss";
 //Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+// util
+import chillHop from "./util";
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
